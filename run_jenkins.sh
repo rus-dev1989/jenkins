877c131556ddbe526688c3c734b742ffdb87103d
rus-dev1989 <37639092+rus-dev1989@users.noreply.github.com>
2018-05-16 14:56:58 +0300

    Update run_jenkins.sh

--- run_jenkins.sh
+++ run_jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#docker build -t  jenkins-solovei .
-docker rm -f jenkins-solovei
+docker build -t  jenkins-solovei .
+#docker rm -f jenkins-solovei
 docker run -d --name jenkins-solovei -p 8080:8080 -v $(pwd)/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkins-solovei
 
